a946b1cbb6dd81ea5913aeb47b519bcc10f25760
David Jensen <tsourdox@icloud.com>
2022-09-10 20:37:31 +0200

    build project properly for npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   test: {
     globals: false,
     environment: 'happy-dom',
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/hooks'),
+      name: 'UsefulStateHooks',
+      fileName: 'index',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react'],
+    },
   },
 });
